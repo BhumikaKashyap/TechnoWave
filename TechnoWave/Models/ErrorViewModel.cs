368676d7a19e503c70a269c3924ee44573c1b9c8
BhumikaKashyap <aashikashyap5477@gmail.com>
2025-11-05 00:08:38 +0530

    add new project here MVC View

---
+++ TechnoWave/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechnoWave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
