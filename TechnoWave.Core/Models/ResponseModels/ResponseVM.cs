6390758a4371b421d22436f9f572f8ce3e2675a6
BhumikaKashyap <aashikashyap5477@gmail.com>
2025-11-04 23:59:43 +0530

    add ResponseVM File

---
+++ TechnoWave.Core/Models/ResponseModels/ResponseVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnoWave.Core.Models.ResponseModels
+{
+    public class ResponseVm<T> : IResponseVm<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+        public int TotalRecords { get; set; }
+    }
+    public interface IResponseVm<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
